d1b999ade39c2f63acc5e09c7170a9dbbcd52116
joncarriere <jonathan.carriere@wanadoo.fr>
2021-01-12 16:00:49 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+require 'controller.php'
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Ocordo</title>
+</head>
+
+<body>
+    <nav id="navBar">
+        <ul>
+            <?php foreach ($xml->page as $page)
+            { ?>
+                <li><a href="?id=<?= $page['id'] - 1; ?>" target="_self"><?= $page->menu; ?></a></li>
+                <?php } ?>
+        </ul>
+    </nav>
+    <?= $xml->page[(int)$_GET['id']]->content ?>
+</body>
+
+</html>
+
+
+<?php
