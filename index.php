f3313788a003470189c5ec2f41404cb06000e3d8
Rémi Bortoluzzi <remi.bortoluzzi@gmail.com>
2021-01-13 15:55:28 +0100

    changes

--- index.php
+++ index.php
@@ -7,11 +7,7 @@ require('controller.php');
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<<<<<<< HEAD
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-=======
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
->>>>>>> d3921a5e51b9fb168fc26cc983098e9385783f2e
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Ocordo</title>
 </head>
