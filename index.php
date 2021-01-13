d3921a5e51b9fb168fc26cc983098e9385783f2e
Max <maxblanck@outlook.fr>
2021-01-13 14:14:02 +0100

    new

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require('controller.php');
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Ocordo</title>
 </head>
@@ -14,44 +15,38 @@ require('controller.php');
 <body>
     <nav id="navBar">
         <ul>
-            <?php foreach ($xml->page as $page)
-            { ?>
+            <?php foreach ($xml->page as $page) { ?>
                 <li><a href="<?= $page['id'] ?>.html" target="_self"><?= $page->menu; ?></a></li>
             <?php } ?>
         </ul>
     </nav>
     <?php
-    if (!empty($_GET['id']) || isset($_GET['id']))
-    {
-        if (is_numeric($_GET['id']))
-        {
-            if (in_array($_GET['id'], $idControl))
-            {
+    if (!empty($_GET['id']) || isset($_GET['id'])) {
+        if (is_numeric($_GET['id'])) {
+            if (in_array($_GET['id'], $idControl)) {
                 echo $xml->page[$_GET['id'] - 1]->content;
-            }
-            else
-            {
+            } else {
                 $error =  'La page recherchée n\'existe pas';
             }
-        }
-        else
-        {
+        } else {
             $error =  'La page recherchée n\'existe pas';
         }
-    }
-    else
-    {
-        if (!$_GET)
-        {
+    } else {
+        if (!$_GET) {
             echo $xml->page[0]->content;
-        }
-        else
-        {
+        } else {
             $error =  'La page recherchée n\'existe pas';
         }
     }
     ?>
-    <p><?= $error?></p>
+    <div class="container">
+        <div class="row ">
+            <div class="col-12 text-center">
+                <p class="text-alert warning"><?= $error ?></p>
+            </div>
+        </div>
+    </div>
+
 </body>
 
 </html>
