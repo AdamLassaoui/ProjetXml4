c3dabbe02256bb0e0e2d5be34a9b761f2d2df053
Max <maxblanck@outlook.fr>
2021-01-14 09:28:39 +0100

    update

--- index.php
+++ index.php
@@ -8,13 +8,13 @@ require('controller.php');
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css" />
     <title>Ocordo</title>
 </head>
 
 <body>
     <nav id="navBar" class="navbar navbar-expand-lg mb-5 navbar-dark">
-        <a class="navbar-brand" href="http://projet-php/1.html">Maçonnerie Ocordo</a>
+        <a class="navbar-brand" href="http://projet-php-groupe/1.html">Maçonnerie Ocordo</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -33,16 +33,16 @@ require('controller.php');
             if (in_array($_GET['id'], $idControl)) {
                 echo $xml->page[$_GET['id'] - 1]->content;
             } else {
-                $error =  'La page recherchée n\'existe pas';
+                $error =  'La page recherchée n\'existe pas' . '<img src="assets/img/Sourire-du-plombier-1.jpg" alt="" class="p-5">';
             }
         } else {
-            $error =  'La page recherchée n\'existe pas';
+            $error =  'La page recherchée n\'existe pas' . '<img src="assets/img/Sourire-du-plombier-1.jpg" alt="" class="p-5">';
         }
     } else {
         if (!$_GET) {
             echo $xml->page[0]->content;
         } else {
-            $error =  'La page recherchée n\'existe pas';
+            $error =  'La page recherchée n\'existe pas' . '<img src="assets/img/Sourire-du-plombier-1.jpg" alt="" class="p-5">';
         }
     }
     ?>
@@ -50,8 +50,7 @@ require('controller.php');
         <div class="row ">
             <div class="col-12 text-center">
                 <p class="text-alert warning">
-                <?= $error ?>
-                <img src="assets/img/Sourire-du-plombier-1.jpg" alt="" class="p-5"></p>
+                <?= $error ?></p>
             </div>
         </div>
     </div>
