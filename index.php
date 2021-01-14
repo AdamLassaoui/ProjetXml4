7daf240e640ff715540b9a208e49ae6fab971974
joncarriere <jonathan.carriere@wanadoo.fr>
2021-01-14 09:16:56 +0100

    modif accueil

--- index.php
+++ index.php
@@ -14,12 +14,12 @@ require('controller.php');
 
 <body>
     <nav id="navBar" class="navbar navbar-expand-lg mb-5 navbar-dark">
-        <a class="navbar-brand" href="#">Maçonnerie Ocordo</a>
-        <button id="" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="http://projet-php/1.html">Maçonnerie Ocordo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mt-2 ml-auto">
                 <?php foreach ($xml->page as $page)
                 { ?>
                     <li class="nav-item"><a class="nav-link" href="<?= $page['id']; ?>.html" target="_self"><?= $page->menu; ?></a></li>
@@ -53,7 +53,8 @@ require('controller.php');
             </div>
         </div>
     </div>
-
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
 
 </html>
